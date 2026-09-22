20c973a6598c47842b3bab1736e4f4035a835ce6
Skokoo <nourriwilliam789@gmail.com>
2026-09-22 19:19:39 +0700

    cool

--- func/sys/syscall.h
+++ func/sys/syscall.h
@@ -29,5 +29,23 @@ long sysread(long fd, char* buf, unsigned long count) {
     return ret;
 }
 
+long sysopen(const char* path, int flags, int mode) {
+    long ret;
+    register const char* r0 asm("x0") = path;
+    register long r1 asm("x1") = flags;
+    register long r2 asm("x2") = mode;
+    register long r8 asm("x8") = 56;
+    asm volatile("svc #0" : "=r"(ret) : "r"(r0), "r"(r1), "r"(r2), "r"(r8) : "memory");
+    return ret;
+}
+
+long sysclose(unsigned int fd) {
+    long ret;
+    register unsigned int r0 asm("x0") = fd;
+    register long r8 asm("x8") = 57;
+    asm volatile("svc #0" : "=r"(ret) : "r"(r0), "r"(r8) : "memory");
+    return ret;
+}
+
 }
 #endif
